b32f2aecd53d68f1973425e707dd1e00e7d37dc7
Martin Brettschneider <martin.brettschneider@gmail.com>
2023-02-13 11:41:44 +0100

    feat: support middlewares testing

--- src/ControllerTester.php
+++ src/ControllerTester.php
@@ -7,8 +7,11 @@ use Apitte\Core\Dispatcher\IDispatcher;
 use Apitte\Core\ErrorHandler\IErrorHandler;
 use Apitte\Core\Http\ApiRequest;
 use Apitte\Core\Http\ApiResponse;
+use Contributte\Middlewares\Application\IApplication;
 use Contributte\Psr7\Psr7Response;
 use Contributte\Psr7\Psr7ServerRequest;
+use Exception;
+use Psr\Http\Message\ResponseInterface;
 use Throwable;
 
 class ControllerTester
@@ -18,10 +21,13 @@ class ControllerTester
 
 	private IErrorHandler $errorHandler;
 
-	public function __construct(IDispatcher $dispatcher, IErrorHandler $errorHandler)
+	private ?IApplication $middlewaresApplication = null;
+
+	public function __construct(IDispatcher $dispatcher, IErrorHandler $errorHandler, ?IApplication $middlewaresApplication = null)
 	{
 		$this->dispatcher = $dispatcher;
 		$this->errorHandler = $errorHandler;
+		$this->middlewaresApplication = $middlewaresApplication;
 	}
 
 	public function createRequest(string $uri): TestControllerRequest
@@ -33,6 +39,18 @@ class ControllerTester
 	{
 		$request = $this->createApiRequest($testControllerRequest);
 		$response = new ApiResponse(new Psr7Response());
+
+		if ($this->middlewaresApplication !== null && method_exists($this->middlewaresApplication, 'runWith')) {
+			ob_start();
+			$response = $this->middlewaresApplication->runWith($request);
+			ob_end_clean();
+
+			if (!$response instanceof ResponseInterface) {
+				throw new Exception(sprintf('Unsupported type of response: \'%s\'.', gettype($response)));
+			}
+
+			return new TestControllerResult(new ApiResponse($response));
+		}
 
 		try {
 			$response = $this->dispatcher->dispatch($request, $response);
